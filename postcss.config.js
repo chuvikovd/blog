4d262e4903d07a37089c740731c9b5c2519d448e
chuvikovd <chuvikovd@gmail.com>
2020-01-14 23:23:22 +0200

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -12,6 +12,6 @@ module.exports = {
     atImport,
     tailwindcss,
     autoprefixer,
-    ...(process.env.NODE_ENV !== 'production' ? [purgecss] : []),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 }
