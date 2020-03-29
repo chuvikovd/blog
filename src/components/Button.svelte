e4b2f90b5d32b145477453e1ee5470830528bc74
Dmitrijs Cuvikovs Private <chuvikovd@gmail.com>
2020-03-29 15:14:52 +0300

    links to projects made external

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,7 +1,7 @@
 <script>
   import { day } from '../store.js'
 
-  export let href
+  export let href, external = false
 
   const toggle = () => day.update(day => !day)
 </script>
@@ -9,10 +9,19 @@
 <a
   rel="prefetch"
   {href}
-  class="inline-block mt-4 p-2 font-display font-bold transition-button relative
+  target={external ? "_blank" : null}
+  class="inline-flex items-center mt-4 p-2 font-display font-bold transition-button relative
   z-10 before:empty-content before:absolute before:border-2 before:border-red
   before:inset-0 after:empty-content after:absolute after:bg-cyan after:inset-0
   after:-z-1 text-black">
   <slot />
-  →
+  {#if external}
+    <svg width="12" height="12" viewBox="0 0 24 24" class="ml-2">
+      <path
+        d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977
+        7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z" />
+    </svg>
+  {:else}
+    →
+  {/if}
 </a>
--- src/components/ProjectHeader.svelte
+++ src/components/ProjectHeader.svelte
@@ -7,13 +7,19 @@
 </script>
 
 <article class="w-full mt-8 first:mt-0">
-  <a href={project.url}>
+  <a href={project.url} class="flex items-center" target="_blank">
     <h2
-      class="font-display text-3xl md:text-4xl font-bold text-red
-      leading-tight mb-1">
+      class="font-display text-3xl md:text-4xl font-bold text-red leading-tight
+      mb-1 mr-4">
       {project.title}
     </h2>
+    <svg width="24" height="24" viewBox="0 0 24 24">
+      <path
+        class="fill-pink"
+        d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977
+        7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z" />
+    </svg>
   </a>
   <p class="mb-4">{project.description}</p>
-  <Button href={project.url}>Learn more</Button>
+  <Button href={project.url} external>Learn more</Button>
 </article>
